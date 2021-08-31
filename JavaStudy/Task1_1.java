ed39c4421b2fb2e801baac21f633da99a7feb2c7
Tachibana-Kazuya <89580685+Tachibana-Kazuya@users.noreply.github.com>
2021-09-01 00:55:50 +0900

    課題提出テキスト
    Task1_1 データ型・文字列クラス

---
+++ JavaStudy/Task1_1.java
@@ -0,0 +1,25 @@
+public class Task1_1{
+
+    public static void main(String[] args) {
+
+        int hogeInt = 50;
+        
+        String hogeString = "テスト";
+        
+        //double型を宣言して、変数名はhogeStringで0.2で初期化
+        double hogeDouble = 0.2;
+
+        hogeInt = 100;
+
+        hogeString = "合格";
+        
+        System.out.println(hogeInt);
+
+        //hogeStringを表示するように指示している
+        System.out.println(hogeString);
+
+        System.out.println(hogeDouble);
+        
+
+    }
+}
