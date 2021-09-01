3ae95cb785ec20bd5afc885af05705efcdcd3a60
Tachibana-Kazuya <89580685+Tachibana-Kazuya@users.noreply.github.com>
2021-09-01 22:54:03 +0900

    課題提出テスト
    Task1_3　演算子

---
+++ JavaStudy/Task1_3.java
@@ -0,0 +1,30 @@
+public class Task1_3 {
+
+    public static void main(String[] args) {
+
+        int plus = 0;
+        plus = 5 + 10;
+
+        int minus = 0;
+        minus = 20 - 7;
+
+        //10と5を乗算した結果（20)を、変数multiplyに代入している
+        int multiply = 10 * 2;
+
+        int remi = 20;
+        remi %= 6;
+
+        //"有楽町"を変数townに代入、"線"を変数lineに代入している
+        String town = "有楽町";
+        String line = "線";
+
+        String train = town + line;
+
+        System.out.println(plus);
+        System.out.println(minus);
+        System.out.println(multiply);
+        System.out.println(remi);
+        System.out.println(train);
+        
+    }
+}
